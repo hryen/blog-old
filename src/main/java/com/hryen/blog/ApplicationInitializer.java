ae2ec4dcd2bb5c8df751f12ea51ff18ffc224fdc
Henry <henrychenchn@gmail.com>
2019-02-11 19:58:43 +0800

    0.1

--- src/main/java/com/hryen/blog/ApplicationInitializer.java
+++ src/main/java/com/hryen/blog/ApplicationInitializer.java
@@ -6,6 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
 
 @Component
 public class ApplicationInitializer implements CommandLineRunner {
@@ -25,6 +28,15 @@ public class ApplicationInitializer implements CommandLineRunner {
 //        } catch (Exception e) {
 //            throw new RuntimeException("Clear redis failed");
 //        }
+
+
+        // create upload directory
+        try {
+            File uploadParent = ResourceUtils.getFile("classpath:");
+            File upload = new File(uploadParent, "upload");
+            upload.mkdir();
+            logger.info("create upload directory Successful!");
+        } catch (Exception e) { logger.error("create upload directory failed!"); }
 
     }
 
