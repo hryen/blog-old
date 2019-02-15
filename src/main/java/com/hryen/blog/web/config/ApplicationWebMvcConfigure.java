550d27b0a21240617b69b048c9b4a55a6d592efa
Henry <henrychenchn@gmail.com>
2019-02-15 19:04:47 +0800

    rename webmvconfigure file

--- src/main/java/com/hryen/blog/web/config/ApplicationWebMvcConfigure.java
+++ src/main/java/com/hryen/blog/web/config/ApplicationWebMvcConfigure.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AdminWebMvcConfigure implements WebMvcConfigurer {
+public class ApplicationWebMvcConfigure implements WebMvcConfigurer {
 
     @Autowired
     private AdminInterceptorHandler adminInterceptorHandler;
