b44352e9125cdb3b70f418c907e81bd58d937fb1
Henry <henrychenchn@gmail.com>
2019-02-25 03:10:29 +0800

    完善登录功能

--- src/main/java/com/hryen/blog/web/admin/AdminLoginController.java
+++ src/main/java/com/hryen/blog/web/admin/AdminLoginController.java
@@ -1,5 +1,6 @@
 package com.hryen.blog.web.admin;
 
+import com.hryen.blog.model.Result;
 import com.hryen.blog.model.User;
 import com.hryen.blog.service.AdminUserService;
 import eu.bitwalker.useragentutils.UserAgent;
@@ -7,14 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Controller
@@ -62,8 +61,13 @@ public class AdminLoginController {
 
     // 接收前台的登录表单 进行登录操作
     @PostMapping("/login")
-    public String login(String username, String password, HttpServletRequest request,
+    @ResponseBody
+    public Result login(@RequestBody Map data, HttpServletRequest request,
                         HttpServletResponse response, Model model) {
+
+        String username = (String) data.get("username");
+        String password = (String) data.get("password");
+        boolean remember = (boolean) data.get("remember");
 
         // 调用service登录的方法
         User user = adminUserService.login(username, password);
@@ -73,47 +77,49 @@ public class AdminLoginController {
             // 1.将user加入session
             request.getSession().setAttribute("user", user);
 
-            // 获取UserAgent 里面包含id 系统信息 浏览器信息等
-            UserAgent userAgent = UserAgent.parseUserAgentString(request.getHeader("User-Agent"));
+            // 如果使用了remember me功能 向客户端发放cookie
+            if (remember) {
+                // 获取UserAgent 里面包含id 系统信息 浏览器信息等
+                UserAgent userAgent = UserAgent.parseUserAgentString(request.getHeader("User-Agent"));
 
-            /**
-             * 用户在redis中存在方式
-             *
-             * 1.登录成功 发放一个cookie 并将该useragent的id放入redis 并设置过期时间和cookie一样
-             *
-             * 例如 cookie是 UID:3   |   redis是 blog_user_agent_id_35131137:3
-             *
-             * cookie U代表是user id   |   redis的key是blog_user_agent_id_ + useragent.id value是user.id
-             *
-             * 2.验证cookie的步骤
-             *     1.获取请求的cookie的uid和userAgent.id
-             *     2.根据该agent.id去redis里查找 如果没找到 代表cookie被复制 因为redis里的过期时间和cookie里一样
-             *       如果cookie存在 redis里不存在 那一定是有问题的
-             *     3.验证cookie的uid和redis的agent.id的value是否一样 一样则直接将该用户放入session
-             */
+                /**
+                 * 用户在redis中存在方式
+                 *
+                 * 1.登录成功 发放一个cookie 并将该useragent的id放入redis 并设置过期时间和cookie一样
+                 *
+                 * 例如 cookie是 UID:3   |   redis是 blog_user_agent_id_35131137:3
+                 *
+                 * cookie U代表是user id   |   redis的key是blog_user_agent_id_ + useragent.id value是user.id
+                 *
+                 * 2.验证cookie的步骤
+                 *     1.获取请求的cookie的uid和userAgent.id
+                 *     2.根据该agent.id去redis里查找 如果没找到 代表cookie被复制 因为redis里的过期时间和cookie里一样
+                 *       如果cookie存在 redis里不存在 那一定是有问题的
+                 *     3.验证cookie的uid和redis的agent.id的value是否一样 一样则直接将该用户放入session
+                 */
 
-            // 2.将userAgent.id放入redis
-            stringRedisTemplate.boundValueOps("blog_user_agent_id_" + userAgent.getId())
-                    .set(String.valueOf(user.getId()), 7L, TimeUnit.DAYS);
+                // 2.将userAgent.id放入redis
+                stringRedisTemplate.boundValueOps("blog_user_agent_id_" + userAgent.getId())
+                        .set(String.valueOf(user.getId()), 7L, TimeUnit.DAYS);
 
-            // 设置uid cookie
-            Cookie cookie = new Cookie("UID", user.getId());
-            // 7天
-            cookie.setMaxAge(60*60*24*7);
-            // cookie只能使用https协议发送给服务器
-            /// cookie.setSecure(true);
-            // HttpOnly可以防止js脚本读取cookie信息 防止XSS攻击
-            cookie.setHttpOnly(true);
+                // 设置uid cookie
+                Cookie cookie = new Cookie("UID", user.getId());
+                // 7天
+                cookie.setMaxAge(60*60*24*7);
+                // cookie只能使用https协议发送给服务器
+                /// cookie.setSecure(true);
+                // HttpOnly可以防止js脚本读取cookie信息 防止XSS攻击
+                cookie.setHttpOnly(true);
 
-            // 3.发放cookie
-            response.addCookie(cookie);
+                // 3.发放cookie
+                response.addCookie(cookie);
+            }
 
-            //跳转到admin首页
-            return "redirect:/admin";
+            // 返回登录成功
+            return new Result(true, "Successful");
         } else {
-            // 如果登录失败 加入提示信息 返回登录页面
-            model.addAttribute("message", "Login failed");
-            return "admin/login";
+            // 返回登录失败
+            return new Result(false, "Username or password is incorrect");
         }
     }
 
--- src/main/resources/templates/admin/login.ftl
+++ src/main/resources/templates/admin/login.ftl
@@ -1,24 +1,89 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Login</title>
-    <style>
-        .input {
-            width: 200px;
-            height: 20px;
-            margin: 2px 0;
-        }
-    </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="${request.contextPath}/css/element-ui.min.css">
+    <script src="${request.contextPath}/js/vue.min.js"></script>
+    <script src="${request.contextPath}/js/element-ui.min.js"></script>
+    <script src="${request.contextPath}/js/axios.min.js"></script>
+    <title>Sign in</title>
 </head>
-<body>
-<form action="${request.contextPath}/admin/login" method="post">
-    <#if message??><p style="color: red;margin: 3px 0;">${message}</p></#if>
-    <input class="input" type="text" name="username" placeholder="Username" autofocus>
-    <br>
-    <input class="input" type="password" name="password" placeholder="Password">
-    <br>
-    <input type="submit" value="submit">
-    <input type="reset" value="reset">
-</form>
+<body style="margin: 0;">
+<div id="app">
+    <el-container>
+        <el-main id="main" style="display: flex;">
+            <div style="width: 360px;margin: auto;">
+                <el-card class="box-card" style="padding: 40px 0 20px 0;">
+                    <div slot="header" class="clearfix"><span>Sign in</span></div>
+
+                    <el-input v-model="form.username" type="text" placeholder="Username"
+                              autofocus style="margin-bottom: 20px;">
+                    </el-input>
+
+                    <el-input v-model="form.password" type="password" placeholder="Passowrd"
+                              style="margin-bottom: 20px;">
+                    </el-input>
+
+                    <el-checkbox v-model="form.remember" style="margin-bottom: 20px;">Remember me</el-checkbox>
+
+                    <el-button type="primary" @click="formSubmit" style="width: 100%;">Sign in</el-button>
+                </el-card>
+            </div>
+
+        </el-main>
+    </el-container>
+</div>
+
+<script>
+    function setMainHeight() {
+        var main = document.getElementById('main');
+        var clientHeight = document.documentElement.clientHeight;
+        main.style.height = clientHeight+'px';
+    }
+
+    setMainHeight();
+
+    window.onresize = function () { setMainHeight(); }
+</script>
+
+<script>
+    Vue.prototype.$axios = axios.create({ baseURL: '${request.contextPath}' });
+
+    var Main = {
+        data() {
+            return {
+                form: {
+                    username: '',
+                    password: '',
+                    remember: true
+                }
+            }
+        },
+
+        mounted() {},
+
+        methods: {
+            formSubmit: function () {
+                this.$axios.post('/admin/login', {
+                    username: this.form.username,
+                    password: this.form.password,
+                    remember: this.form.remember
+                }).then((response) => {
+                    console.log(response);
+                if (response.data.result) {
+                    window.location.href = "/admin/index";
+                } else {
+                    this.$message({type: 'error', message: response.data.message});
+                }
+            }).catch((error) => { console.log(error); });
+            }
+        }
+    };
+
+    var Ctor = Vue.extend(Main);
+    new Ctor().$mount('#app');
+</script>
+
 </body>
 </html>
