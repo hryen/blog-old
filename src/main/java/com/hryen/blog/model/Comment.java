161de7342d9da3958ed9e99cc6ff510baff51663
Henry <henrychenchn@gmail.com>
2019-03-18 00:53:16 +0800

    解决Infinite recursion异常

--- src/main/java/com/hryen/blog/model/Comment.java
+++ src/main/java/com/hryen/blog/model/Comment.java
@@ -1,6 +1,7 @@
 package com.hryen.blog.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -22,6 +23,7 @@ public class Comment implements Serializable {
 
     private String parentId;
 
+    @JsonIgnore
     private Comment parentComment;
 
     private String author;
