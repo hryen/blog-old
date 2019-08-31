6e132b23ca922c4a2789ed9ff8b3aca04c7f7036
hryen <hryen9@gmail.com>
2019-08-31 23:00:52 +0800

    去除控制台显示端口信息

--- src/main/java/com/hryen/blog/Application.java
+++ src/main/java/com/hryen/blog/Application.java
@@ -1,27 +1,17 @@
 package com.hryen.blog;
 
-import com.hryen.blog.util.Snowflake;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.Bean;
+import com.hryen.blog.util.Snowflake;
 
 @EnableCaching
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-
-        Logger logger = LoggerFactory.getLogger(Application.class);
-
-        ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-
-        String serverPort = run.getEnvironment().getProperty("server.port");
-
-        logger.info("Application is running at http://localhost:" + serverPort);
+        SpringApplication.run(Application.class, args);
     }
 
     @Bean
